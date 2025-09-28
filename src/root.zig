1ea05150e05719adf619272fda2ab3cce81cbf78
Ondrej Stanek <viego443@proton.me>
2025-09-28 11:13:38 +0200

    add binary search

--- src/root.zig
+++ src/root.zig
@@ -1,13 +1,60 @@
-//! By convention, root.zig is the root source file when making a library. If
-//! you are making an executable, the convention is to delete this file and
-//! start with main.zig instead.
 const std = @import("std");
 const testing = std.testing;
 
-pub export fn add(a: i32, b: i32) i32 {
-    return a + b;
+/// The internal function used by the implementation. You probably don't want this unless
+/// you only want to search in a specific part of the sorted array. See [`binSearch`]
+/// instead.
+pub fn rawBinSearch(comptime T: type, target: T, arr: []const T, start: usize, end: usize) ?usize {
+    if (start > end) {
+        return null;
+    }
+
+    const diff: f32 = @as(f32, @floatFromInt(end)) - @as(f32, @floatFromInt(start));
+    const m: usize = @intFromFloat(@as(f32, @floatFromInt(start)) + std.math.floor(diff / 2));
+
+    if (arr[m] < target) {
+        return rawBinSearch(T, target, arr, m + 1, end);
+    }
+
+    if (arr[m] == target) {
+        return m;
+    }
+
+    if (arr[m] > target) {
+        if (m == 0) {
+            return null;
+        }
+
+        return rawBinSearch(T, target, arr, start, m - 1);
+    }
+
+    unreachable;
 }
 
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
+/// Perform binary search on array `arr`, returning the `target`'s index if found, or
+/// returning `null` otherwise.
+pub fn binSearch(comptime T: type, target: T, arr: []const T) ?usize {
+    if (arr.len == 0) {
+        return null;
+    }
+
+    return rawBinSearch(T, target, arr, 0, arr.len);
+}
+
+test "search for an element" {
+    const myArr = [_]u8{ 1, 2, 3, 5, 6, 7 };
+
+    try testing.expect(binSearch(u8, 3, &myArr) == 2);
+}
+
+test "search for a non-existent element" {
+    const myArr = [_]u8{ 4, 5, 6 };
+
+    try testing.expect(binSearch(u8, 3, &myArr) == null);
+}
+
+test "search in an empty slice" {
+    const myArr = [_]u8{};
+
+    try testing.expect(binSearch(u8, 5, &myArr) == null);
 }
